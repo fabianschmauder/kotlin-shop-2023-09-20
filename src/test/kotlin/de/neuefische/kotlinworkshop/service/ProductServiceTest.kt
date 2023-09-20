1fe973bc2eb63bb1d4d5b5dffbb169f9087eb63e
Fabian Schmauder <fabian.schmauder@gmail.com>
2023-09-20 15:02:59 +0200

    Add test get by id

--- src/test/kotlin/de/neuefische/kotlinworkshop/service/ProductServiceTest.kt
+++ src/test/kotlin/de/neuefische/kotlinworkshop/service/ProductServiceTest.kt
@@ -2,7 +2,8 @@ package de.neuefische.kotlinworkshop.service
 
 import de.neuefische.kotlinworkshop.domain.Product
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.containsInAnyOrder
+import org.hamcrest.Matchers.*
+import org.hamcrest.Matchers.`is` as Is
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,6 +21,20 @@ class ProductServiceTest {
 
         assertThat(
             result, containsInAnyOrder(
+                Product(
+                    id = "id1",
+                    name = "Super Product"
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `get by id should return item wIth id`() {
+        val result = productService.getById("id1")
+
+        assertThat(
+            result, Is(
                 Product(
                     id = "id1",
                     name = "Super Product"
