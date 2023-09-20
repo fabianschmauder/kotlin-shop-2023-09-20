c909b932dabf4549a2b34a8c0ba6ae719ac9d967
Fabian Schmauder <fabian.schmauder@gmail.com>
2023-09-20 12:59:56 +0200

    Init kotlin spring

---
+++ src/test/kotlin/de/neuefische/kotlinworkshop/KotlinWorkshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.neuefische.kotlinworkshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWorkshopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
